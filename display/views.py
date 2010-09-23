839a548f7fc375e12de6795944d431c77045336e
Seth Woodworth <seth@isforinsects.com>
2010-09-23 00:24:17 -0400

    starting display config and url rewrite

--- display/views.py
+++ display/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from dashboard.display.models import Spreadsheet
+
+def index(request):
+     = Poll.objects.all().order_by('-pub_date')[:5]
+    return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
+
--- urls.py
+++ urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     # Example:
-    # (r'^dashboard/', include('dashboard.foo.urls')),
+    (r'^dashboard/', include('dashboard.display.urls')),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
