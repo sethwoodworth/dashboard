80daff60d0a098efb22a55ee21d0e2acaa720782
Alex Jarvis <alxjrvs@gmail.com>
2011-01-18 17:05:12 -0500

    merged CSS and Base.html with dashboard

--- pstp/admin.py
+++ pstp/admin.py
@@ -1,20 +1,8 @@
-from pstp.models import Subject, GradedAttr, Subheader 
+from oneville.pstp.models import * 
 from django.contrib import admin
 
-class SubheaderInline(admin.StackedInline):
-    model = Subheader
-    extra = 1
-
-class GradesInline(admin.StackedInline):
-    model = GradedAttr
-    extra = 6
-
-
-class SubjectAdmin(admin.ModelAdmin):
-    fieldsets = [
-        ('Name',                  {'fields':['name']}),
-    ]
-    inlines = [SubheaderInline]
-    inlines = [GradesInline]
-
-admin.site.register(Subject, SubjectAdmin )
+admin.site.register(Subject)
+admin.site.register(Subheader)
+admin.site.register(GradedAttr)
+admin.site.register(CommentField)
+admin.site.register(Grade)
--- pstp/views.py
+++ pstp/views.py
@@ -4,19 +4,16 @@ from pstp.models import  Subject, Subheader, GradedAttr
 def index(request):
 
     sub_dict = {}
-    head_dict = {}
 
     for subject in Subject.objects.all():
-        x = Subheader.objects.filter(subject = subject)
-        sub_dict[subject.name] = x
+        headers_of_subject = Subheader.objects.filter(subject = subject)
+        sub_dict[subject.name] = {}
 
-    for subheader in Subheader.objects.all():
-        x = GradedAttr.objects.filter(subheader = subheader)
-        head_dict[subheader.name] = x
-# The next line would be used if we were going to automagically generate commentfields. As it stands, I am just going to hardcode them in index.html.     
-#    for subject in CommentField.objects.all
+        for mysubheader in headers_of_subject: 
+            ga_list = GradedAttr.objects.filter(subheader = mysubheader) 
+            sub_dict[subject.name][mysubheader.name] = ga_list
+                
     
     return render_to_response('pstp/index.html',{
         'sub_dict': sub_dict,
-        'head_dict': head_dict,
     })
